d1b7b1a077a6a934560c637e40ab68a6d3abb050
Yomna147 <syomna59@gmail.com>
2023-02-16 11:26:27 -0800

    l

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the size
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %1u byte(s)\n" , (unsigned long)
+sizeof(a));
+printf("Size of a char: %1u byte(s)\n" , (unsigned long)
+sizeof(b));
+printf("Size of a char: %1u byte(s)\n" , (unsigned long)
+sizeof(c));
+printf("Size of a char: %1u byte(s)\n" , (unsigned long)
+sizeof(d));
+return (0)
+}
