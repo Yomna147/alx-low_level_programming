30e43615fb7b5d166cde10e2293b4cf47d599f48
Yomna147 <syomna59@gmail.com>
2023-02-16 11:47:43 -0800

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,13 +13,13 @@ int main(void)
 
 printf("Size of a char: %1u byte(s)\n", (unsigned long)
 sizeof(a));
-printf("Size of a char: %1u byte(s)\n", (unsigned long)
+printf("Size of an int: %1u byte(s)\n", (unsigned long)
 sizeof(b));
-printf("Size of a char: %1u byte(s)\n", (unsigned long)
+printf("Size of a long int: %1u byte(s)\n", (unsigned long)
 sizeof(c));
-printf("Size of a char: %1u byte(s)\n", (unsigned long)
+printf("Size of a long long int: %1u byte(s)\n", (unsigned long)
 sizeof(d));
-printf("Size of a char: %1u byte(s)\n", (unsigned long)
+printf("Size of a float: %1u byte(s)\n", (unsigned long)
 sizeof(f));
 return (0);
 }
