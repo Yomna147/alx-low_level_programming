781318e82af7d0cdb4beaddb3ef4eec3fff7d873
Yomna147 <syomna59@gmail.com>
2023-02-17 07:56:06 -0800

    j

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+nclude <stdio.h>
+
+/**
+ * main -a program that prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+	for(letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+		return (0);
+}
