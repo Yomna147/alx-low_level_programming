680b9ac0c8e1169b3050840dc7fd5cb05f1e187c
Yomna147 <syomna59@gmail.com>
2023-02-21 11:11:38 -0800

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+	       printf("+");
+	       return (1);
+	}
+	else if  (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else  (n < 0)
+	{
+		printf("-")
+		return (-1);
+	}
+}
