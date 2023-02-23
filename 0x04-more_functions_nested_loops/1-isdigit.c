d012ef0424e836471973e87a44ac33d3d1d37fc0
Yomna147 <syomna59@gmail.com>
2023-02-23 06:11:02 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,12 +5,10 @@
  * Return: 1 if the number is a digit, 0 otherwise.
  */
 int _isdigit(int c)
-{   
-    int number = 1;
-    int otherwise = 0;
-    if (c >= '0' && c <= '9')
-		return number;
-
-    else
-		return otherwise;
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
 }
