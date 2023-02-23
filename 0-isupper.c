3eef546257acd271c846dee5dcd2ac8ce0e4bde3
Yomna147 <syomna59@gmail.com>
2023-02-23 02:57:27 -0800

    d

---
+++ 0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks for uppercase character.
+ *
+ * Return:1 if c uppercase and 0 otherwise
+ */
+int _isupper(int c);
+{
+	char c;
+
+    c = 'A';
+    return (1);
+
+    c = 'a';
+    return (0);
+}
