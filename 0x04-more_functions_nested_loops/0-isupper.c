f7178f3e82f7264ab561c4e63d194b433e9eb089
Yomna147 <syomna59@gmail.com>
2023-02-23 03:04:39 -0800

    d

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: 1 if c is uppercase and 0 othereise
+ *
+ */
+
+int _isupper(int c);
+{
+	char c;
+
+if (c >= 'A' && c <= 'Z')
+	return (1);
+else
+	return (0);
+}
