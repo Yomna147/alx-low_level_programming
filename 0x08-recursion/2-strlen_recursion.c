2c2ae1dc3172e5595510c8aeff8ea95b058277e6
Yomna147 <syomna59@gmail.com>
2023-03-08 04:38:40 -0800

    j

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,10 +5,10 @@
  * @s: string
  * Return: the lenth of a string.
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
 	else
-		return(1 + int _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 }
