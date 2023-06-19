dc681c7573b7771e5ab6f435339afbdc4570b188
Yomna147 <syomna59@gmail.com>
2023-06-19 15:49:46 +0000

    D

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp alx-low_level_programming/0x18-dynamic_libraries/library.so
-export LD_PRELOAD=./shared-library.so
+cp alx-low_level_programming/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD=./100-operations.so
